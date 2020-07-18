883983749f5947588f60300203b2fc0ee9eb01c0
서정민 <_seowjdals@seojeongmin-ui-MacBookPro.local>
2020-07-18 11:53:33 +0900

    Bad Word Crawler making_3

--- src/crawler/crawler_ilbe.py
+++ src/crawler/crawler_ilbe.py
@@ -22,51 +22,53 @@ def run_ilbe():
         while Flag:
             page = ilbe_crawler.getPage()
             target_list = page.find("div", {"class": "board-list"}).find("ul").findAll("a", {"class": "subject", "style": None})
-            Flag = ilbe_crawler.makePagelist(target_list, 2000000)
-            ilbe_crawler.changePage(1)
-            BWAI__variable(ilbe_crawler.getDB()).update__variable(len(ilbe_crawler.url_list))
-        
-        for url in ilbe_crawler.url_list:
-            document = {}
-
-            # url 등록
-            document['url'] = url
-
-            # 타깃 접속!
-            target = request_crawler(url)
-            page = target.getPage()
-            time.sleep(2)
             
+            if not target_list:
+                break
             
-            # 제목 크롤
-            title = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-header"}).find("a").get_text(" ", strip = True)
-            document['title'] = title
-            FILE.write("URL: " + url + "\n")
-            FILE.write("제목: " + title + "\n")
-            print("URL: " + url)
-            print("제목: " + title)
+            for url in ilbe_crawler.url_list:
+                document = {}
 
-            # 본문 크롤
-            document['post'] = []
-            post = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-content"}).findAll("p")
-            if post:
-                for p in post:
-                    text = p.get_text(" ", strip = True)
+                # url 등록
+                document['url'] = url
+
+                # 타깃 접속!
+                target = request_crawler(url)
+                page = target.getPage()
+                time.sleep(2)
+                
+                
+                # 제목 크롤
+                title = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-header"}).find("a").get_text(" ", strip = True)
+                document['title'] = title
+                FILE.write("URL: " + url + "\n")
+                FILE.write("제목: " + title + "\n")
+                print("URL: " + url)
+                print("제목: " + title)
+
+                # 본문 크롤
+                document['post'] = []
+                post = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-content"}).findAll("p")
+                if post:
+                    for p in post:
+                        text = p.get_text(" ", strip = True)
+                        if len(text) > 0:
+                            document['post'].append(text)
+                        
+                else:
+                    text = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-content"}).get_text(" ", strip = True)
                     if len(text) > 0:
                         document['post'].append(text)
-                    
-            else:
-                text = page.find("div", {"class": "post-wrap"}).find("div", {"class": "post-content"}).get_text(" ", strip = True)
-                if len(text) > 0:
-                    document['post'].append(text)
 
-            document['join_post'] = ' '.join(document['post'])
+                document['join_post'] = ' '.join(document['post'])
 
-            FILE.write("본문: " + document['join_post'] + "\n")
-            FILE.write("===================================================\n")
-            print("본문: " + document['join_post'])
-            print("===================================================")
+                FILE.write("본문: " + document['join_post'] + "\n")
+                FILE.write("===================================================\n")
+                print("본문: " + document['join_post'])
+                print("===================================================")
 
-            BWAI__posts(target.getDB()).insert__one(document)
+                BWAI__posts(target.getDB()).insert__one(document)
+            
+            ilbe_crawler.changePage(1)
 
     FILE.close()
