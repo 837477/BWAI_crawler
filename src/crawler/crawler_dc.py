a7947145d70e3ad2771f82082a6ae20cdd69f402
서정민 <_seowjdals@seojeongmin-ui-MacBookPro.local>
2020-07-25 19:28:07 +0900

    update

--- src/crawler/crawler_dc.py
+++ src/crawler/crawler_dc.py
@@ -18,50 +18,58 @@ def run_dc():
 
         Flag = True
         while Flag:
-            page = crawler.getPage()
+            try:
+                page = crawler.getPage()
+            except:
+                continue
             time.sleep(3)
-            target = page.find("tbody").findAll("td", {"class": "ub-word"})
+            try:
+                target = page.find("tbody").findAll("td", {"class": "ub-word"})
+            except:
+                continue
             target_list = []
             for t in target:
-                temp = {}
-                temp['href'] = t.find("a")['href']
-                if t.find("a").find("em")['class'][1] == "icon_pic":
-                    temp['type'] = 1
-                else:
-                    temp['type'] = 0
-                target_list.append(temp)
+                try:
+                    temp = {}
+                    temp['href'] = t.find("a")['href']
+                    target_list.append(temp)
+                except:
+                    continue
             
             if not target_list:
                 break
             
             for url in target_list:
-                document = {}
+                try:
+                    document = {}
 
-                # url 등록
-                document['url'] = crawler.getDomain() + url['href']
+                    # url 등록
+                    document['url'] = crawler.getDomain() + url['href']
 
-                # 타깃 접속!
-                target = request_crawler(document['url'])
-                page = target.getPage()
-                time.sleep(3)
-                
-                # 제목 크롤
-                title = page.find("h3", {"class": "title"}).find("span", {"class": "title_subject"}).get_text(" ", strip = True)
-                document['title'] = title
-                print("URL: " + document['url'])
-                print("제목: " + title)
+                    # 타깃 접속!
+                    target = request_crawler(document['url'])
+                    page = target.getPage()
+                    time.sleep(3)
+                    
+                    # 제목 크롤
+                    title = page.find("h3", {"class": "title"}).find("span", {"class": "title_subject"}).get_text(" ", strip = True)
+                    document['title'] = title
+                    print("URL: " + document['url'])
+                    print("제목: " + title)
 
-                # 본문 크롤
-                document['post'] = []
+                    # 본문 크롤
+                    document['post'] = []
 
-                post = page.find("div", {"class": "writing_view_box"})
-                if post:
-                    document['post'].append(post.get_text(" ", strip = True))
-                
-                document['join_post'] = ' '.join(document['post'])
-                print(document['post'])
-                print("본문: " + document['join_post'])
-                print("===================================================")
+                    post = page.find("div", {"class": "writing_view_box"})
+                    if post:
+                        document['post'].append(post.get_text(" ", strip = True))
+                    
+                    document['join_post'] = ' '.join(document['post'])
+                    print(document['post'])
+                    print("본문: " + document['join_post'])
+                    print("===================================================")
+                except:
+                    continue
 
                 BWAI__posts(target.getDB()).insert__one(document)
             
